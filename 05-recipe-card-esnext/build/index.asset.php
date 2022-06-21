5e5e7f2763e58d49df0cbe893b99a3da82f32978
Stephy Miehle <stephy@northxsouth.co>
2022-06-21 17:30:19 -0500

    Calculate Nutrition data

--- 05-recipe-card-esnext/build/index.asset.php
+++ 05-recipe-card-esnext/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '6abfb50f64f6389051f6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '27d3121ae3fe3ca5c6b2');
