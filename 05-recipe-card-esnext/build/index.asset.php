aefe9c87f9c28c52ff75ddc6d2ec2a679ca8aebc
Stephy Miehle <stephy@northxsouth.co>
2022-06-21 20:40:00 -0500

    Run prod build of Recipe Card

--- 05-recipe-card-esnext/build/index.asset.php
+++ 05-recipe-card-esnext/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9e54e201a740e8fe6e54');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4907fd85c2b5b3eb1764');
